6ab487f63010fa07cf50cab5ef188dad7181e56a
vacastro <56000486+vacastro@users.noreply.github.com>
2021-09-30 20:41:14 -0300

    Modulo 1 de la clase

---
+++ src/Modulo1/Ejercicio2.java
@@ -0,0 +1,16 @@
+package Modulo1;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Nombre\t\tapellido\temail\t\t\ttelefono");
+		System.out.println("-------------\t---------\t--------\t\t----------");
+		System.out.println("\n");
+		System.out.println("Veronica\tCastro\t\tveroc@gmail.com\t\t43451236");
+		System.out.println("Alan\t\tNakaganeku\talann@gmail.com\t\t44519966");
+		System.out.println("Rosa\t\tLopez\t\trosal@gmail.com\t\t45613322");
+	}
+
+}
---
+++ src/Modulo1/Ejercicio3.java
@@ -0,0 +1,18 @@
+package Modulo1;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tecla de escape" + "\t\tSignificado");
+		System.out.println("\n");
+		System.out.println("\\n" + "\t\t\tSignifica nueva linea");
+		System.out.println("\\t" + "\t\t\tSignifica un tab de espacio");
+		System.out.println("\\\"" + "\t\t\tEs para poner \" (comillas dobles) dentro del texto por ejemplo \"Belencita\"");
+		System.out.println("\\\\" + "\t\t\tSe utiliza para escribir la \\ dentro del texto por ejemplo \\algo\\");
+		System.out.println("\\\'" + "\t\t\tSe utiliza para las \' (comillas simples) para escribir por ejemplo  \'Princesita\'");
+		
+
+	}
+
+}
